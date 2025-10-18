8b10841eb6957f2b5061608f0631871fb9c46cda
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-10-17 21:48:41 -0500

    base project

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <body className="bg-white text-gray-900">
-        <Header />
+        {/* <Header /> */}
         <main className="pt-16">{children}</main>
       </body>
     </html>

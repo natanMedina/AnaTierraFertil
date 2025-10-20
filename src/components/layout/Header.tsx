6521beaeabf4c1ab6229c2ef9c7ad74b5d560256
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-10-19 21:48:05 -0500

    adding purchase contact dialog to redirect to WhatsApp or cancel

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,11 +3,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from '@/components/ui/dialog'
+import { useState } from 'react'
 
 export default function Header() {
+  const [open, setOpen] = useState(false)
+
   return (
     <header className="w-full border-b border-border bg-background">
       <div className="mx-auto flex items-center justify-between py-4 px-12">
+        {/* Logo */}
         <Link
           href="/"
           className="flex items-center gap-3 hover:opacity-90 transition"
@@ -23,6 +34,7 @@ export default function Header() {
           </span>
         </Link>
 
+        {/* Navigation */}
         <nav className="flex items-center gap-9">
           <Link href="/store" className="hover:text-primary transition">
             Productos
@@ -37,11 +49,49 @@ export default function Header() {
             Novedades
           </Link>
 
-          <Button asChild className="bg-brand hover:bg-brand/80 text-white">
-            <a>Contacto</a>
+          {/* Contact Button */}
+          <Button
+            onClick={() => setOpen(true)}
+            className="bg-brand hover:bg-brand/80 text-white"
+          >
+            Contacto
           </Button>
         </nav>
       </div>
+      {/* Dialog */}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-md bg-brand py-10">
+          <DialogHeader>
+            <DialogTitle className="text-center text-background text-xl">
+              Serás redirigido a WhatsApp para comunicarte con Ana Tierra Fértil
+            </DialogTitle>
+          </DialogHeader>
+
+          <p className="text-center text-gray-50">¿Deseas continuar?</p>
+
+          <DialogFooter className="pt-6">
+            <div className="w-full flex items-center justify-center gap-4">
+              <Button
+                onClick={() => {
+                  setOpen(false)
+                  window.open('https://wa.me/573001234567', '_blank')
+                }}
+                variant="outline"
+                className="bg-background hover:bg-background/60 text-brand"
+              >
+                Seguro
+              </Button>
+
+              <Button
+                onClick={() => setOpen(false)}
+                className="bg-background hover:bg-background/60 text-brand"
+              >
+                Por ahora no
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </header>
   )
 }

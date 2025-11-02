a48385ecd927c3b47d0f76201cf6d93ffd5f05ec
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-11-02 15:23:04 -0500

    using id in product detail page

--- src/app/(public)/products/[id]/page.tsx
+++ src/app/(public)/products/[id]/page.tsx
@@ -4,8 +4,8 @@ import UnderConstruction from '@/components/temp/UnderConstruction.tsx'
 import { useParams } from 'next/navigation'
 
 export default function ProductDetailPage() {
-  const params = useParams();
-  const id = params.id;
+  const params = useParams()
+  const id = params.id
 
-  return <UnderConstruction section={"Detalle del producto: " + id} />
+  return <UnderConstruction section={'Detalle del producto: ' + id} />
 }

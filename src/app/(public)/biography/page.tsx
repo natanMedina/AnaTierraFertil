7133a846a5b9f03fe21006ca171c55d03527b8f1
MiguelTami <miguel.tami@correounivalle.edu.co>
2025-11-11 11:50:12 -0500

    Update social links and icons in biography and footer
    Replaced hardcoded social URLs and SVGs in the biography page with config-driven links and external SVG files for Instagram, Blog, and Linktree. Added new SVG assets for Blogger and Linktree, removed the old blog SVG, and updated the footer to link to Instagram instead of mailto. Introduced a 'redes' section in siteConfig for centralized social URLs.

--- src/app/(public)/biography/page.tsx
+++ src/app/(public)/biography/page.tsx
@@ -102,63 +102,61 @@ const SocialSection = () => (
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-8">
           {/* Instagram */}
           <a
-            href="https://www.instagram.com/ana.tierrafertil"
+            href={siteConfig.redes.instagram}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-4 group"
           >
             <div className="w-12 h-12 flex items-center justify-center transition-transform group-hover:scale-110">
-              <svg
-                width="48"
-                height="48"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-                  fill="#0F0F0F"
-                />
-                <path
-                  d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
-                  fill="#0F0F0F"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
-                  fill="#0F0F0F"
-                />
-              </svg>
+              <img
+                src="/instagram-svgrepo-com.svg"
+                alt="Instagram"
+                width={48}
+                height={48}
+              />
             </div>
             <span className="text-lg font-medium text-gray-800 group-hover:text-brand transition-colors">
-              @ana.tierrafertil
+              {siteConfig.homeBiography.username}
             </span>
           </a>
 
           {/* Blog */}
-          <a href="/news" className="flex items-center gap-4 group">
+          <a
+            href={siteConfig.redes.blog}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-4 group"
+          >
             <div className="w-12 h-12 flex items-center justify-center transition-transform group-hover:scale-110">
-              <svg
-                width="48"
-                height="48"
-                viewBox="0 0 512 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M416.428,0.004H95.58C42.787,0.013,0.016,42.792,0,95.577v303.685
-                  c0.025,62.262,50.463,112.717,112.742,112.734h286.524c62.27-0.017,112.717-50.464,112.734-112.734V95.577
-                  C511.992,42.792,469.212,0.013,416.428,0.004z M464.805,399.262c-0.008,18.15-7.308,34.424-19.198,46.34
-                  c-11.916,11.891-28.19,19.19-46.34,19.198H112.742c-18.15-0.009-34.433-7.308-46.348-19.198
-                  c-11.892-11.916-19.182-28.19-19.198-46.34V118.696h417.61V399.262z"
-                  fill="#0F0F0F"
-                />
-              </svg>
+              <img
+                src="/blogger-round-svgrepo-com.svg"
+                alt="Blog"
+                width={48}
+                height={48}
+              />
             </div>
             <span className="text-lg font-medium text-gray-800 group-hover:text-brand transition-colors">
               Explora mi blog
+            </span>
+          </a>
+
+          {/* Linktree */}
+          <a
+            href={siteConfig.redes.linktree}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-4 group"
+          >
+            <div className="w-12 h-12 flex items-center justify-center transition-transform group-hover:scale-110">
+              <img
+                src="/linktree-svgrepo-com.svg"
+                alt="Linktree"
+                width={48}
+                height={48}
+              />
+            </div>
+            <span className="text-lg font-medium text-gray-800 group-hover:text-brand transition-colors">
+              Linktree
             </span>
           </a>
         </div>

f3161f1be9360be94f00825709515ce4ce789e62
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-11-17 01:37:21 -0500

    adding userecordvisit hook in app sections to record its visit for metrics

--- src/app/(public)/services/page.tsx
+++ src/app/(public)/services/page.tsx
@@ -20,8 +20,10 @@ import { Button } from '@/components/ui/button'
 import { useAdmin } from '@/context/AdminContext'
 import { CirclePlus } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { useCreateVisit } from '@/hooks/useRecordVisit'
 
 export default function ServicesPage() {
+  useCreateVisit()
   const { editMode } = useAdmin()
   const router = useRouter()
   const [services, setServices] = useState<Service[]>([])
--- src/hooks/useRecordVisit.ts
+++ src/hooks/useRecordVisit.ts
@@ -1,13 +1,13 @@
 import { useEffect } from 'react'
 import { getDeviceType } from '@/utils/device'
-// import { createVisit } from '@/services/metrics'
+import { createVisit } from '@/services/metrics'
 
 export function useCreateVisit() {
   useEffect(() => {
     const pathname = window.location.pathname
     const device = getDeviceType()
 
-    // createVisit({ device: device, pathname: pathname })
+    createVisit({ device: device, pathname: pathname })
     console.log('Visita creada: ', pathname, device)
   }, [])
 }
